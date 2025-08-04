c313c151afa233dee7a8d1981b4dd16e8a9154a7
Ed Weldon <ed.j.weldon@gmail.com>
2025-08-03 20:44:38 -0400

    Add 3digit country code, Cleaned up Grouping code, Top 10 formatted summaries, World Map

--- draftscript.R
+++ draftscript.R
@@ -15,6 +15,15 @@ colnames(data_salaries)[which(names(data_salaries) == "Salary_In_Usd")] = "Salar
 colnames(data_salaries)[which(names(data_salaries) == "Employee_Residence")] = "Employee_Residence_Code"
 colnames(data_salaries)[which(names(data_salaries) == "Company_Location")] = "Company_Location_Code"
 colnames(data_salaries)[which(names(data_salaries) == "Experience_Level")] = "Experience_Level_Code"
+country_codes_legend = read.csv('continents2.csv')
+country_name_lookup = function(code) {
+  code_match = match(code, country_codes_legend$alpha.2)
+  country_codes_legend$name[code_match]
+}
+country_code3_lookup = function(code2) {
+  code_match = match(code2, country_codes_legend$alpha.2)
+  country_codes_legend$alpha.3[code_match]
+}
 data_salaries = data_salaries %>%
   mutate(
     Experience_Level = factor(
@@ -33,19 +42,12 @@ data_salaries = data_salaries %>%
       levels = c('In-Person', 'Hybrid', 'Remote')),
     International = factor(
       ifelse(Company_Location_Code != Employee_Residence_Code, 'International', 'Domestic'),
-      levels = c('Domestic', 'International'))
-  )
-country_codes_legend = read.csv('continents2.csv')
-country_name_lookup = function(code) {
-  code_match = match(code, country_codes_legend$alpha.2)
-  country_codes_legend$name[code_match]
-}
-data_salaries = data_salaries %>%
-  mutate(
+      levels = c('Domestic', 'International')),
     Employee_Residence_Name = country_name_lookup(Employee_Residence_Code),
-    Company_Location_Name   = country_name_lookup(Company_Location_Code)
+    Company_Location_Name = country_name_lookup(Company_Location_Code),
+    Company_Location_Code3 = country_code3_lookup(Company_Location_Code)
   )
-data_salaries =data_salaries %>% 
+data_salaries = data_salaries %>% 
   select(
     Work_Year,
     Job_Title,
@@ -57,6 +59,7 @@ data_salaries =data_salaries %>%
     Work_Office_Arrangement,
     Company_Size,
     Company_Location_Code,
+    Company_Location_Code3,
     Company_Location_Name,
     Employee_Residence_Code,
     Employee_Residence_Name,
@@ -70,9 +73,10 @@ yearly_job_count = data_salaries %>%
 yearly_job_count
 data_salaries_2024 = data_salaries %>% filter(Work_Year == 2024) %>%
   arrange(desc(Salary_USD))
-data_salaries_2024_select = data_salaries_2024 %>% select(Job_Title, Experience_Level, Work_Time_Arrangement, Work_Office_Arrangement, Company_Size, Company_Location_Name, International, Salary_USD)
+data_salaries_2024_select = data_salaries_2024 %>% filter(Work_Time_Arrangement == 'Full-time') %>% 
+  select(Job_Title, Experience_Level, Work_Time_Arrangement, Work_Office_Arrangement, Company_Size, Company_Location_Code3, Company_Location_Name, International, Salary_USD)
 data_salaries_2024_select_asc = data_salaries_2024_select %>% arrange(Salary_USD)
-Total_Count_2024 = nrow(data_salaries_2024_select)
+Total_Count_2024 = nrow(data_salaries_2024)
 FX_Rates_2024 = data_salaries_2024 %>%
   group_by(Salary_Currency) %>%
   summarise(
@@ -85,148 +89,75 @@ FX_Rates_2024 = data_salaries_2024 %>%
     FX_Rate = Avg_Salary_LOC / Avg_Salary_USD
   ) %>%
   arrange(Salary_Currency)
-Job_Title = data_salaries_2024_select %>%
-  group_by(Job_Title) %>%
-  summarise(
-    Avg_Salary_USD = round(mean(Salary_USD),0), Count = n(), .groups = 'drop') %>%
-  mutate(Percentage = round(Count / Total_Count_2024 * 100, 0)) %>%
-  arrange(desc(Avg_Salary_USD))
-Experience_Level = data_salaries_2024_select %>%
-  group_by(Experience_Level) %>%
-  summarise(
-    Avg_Salary_USD = round(mean(Salary_USD),0), Count = n(), .groups = 'drop') %>%
-  mutate(Percentage = round(Count / Total_Count_2024 * 100, 0)) %>%
-  arrange(desc(Avg_Salary_USD))
-Work_Time_Arrangement = data_salaries_2024_select %>%
-  group_by(Work_Time_Arrangement) %>%
-  summarise(
-    Avg_Salary_USD = round(mean(Salary_USD),0), Count = n(), .groups = 'drop') %>%
-  mutate(Percentage = round(Count / Total_Count_2024 * 100, 0)) %>%
-  arrange(desc(Avg_Salary_USD))
-Work_Office_Arrangement = data_salaries_2024_select %>%
-  group_by(Work_Office_Arrangement) %>%
-  summarise(
-    Avg_Salary_USD = round(mean(Salary_USD),0), Count = n(), .groups = 'drop') %>%
-  mutate(Percentage = round(Count / Total_Count_2024 * 100, 0)) %>%
-  arrange(desc(Avg_Salary_USD))
-Company_Size = data_salaries_2024_select %>%
-  group_by(Company_Size) %>%
-  summarise(
-    Avg_Salary_USD = round(mean(Salary_USD),0), Count = n(), .groups = 'drop') %>%
-  mutate(Percentage = round(Count / Total_Count_2024 * 100, 0)) %>%
-  arrange(desc(Avg_Salary_USD))
-Company_Location_Name = data_salaries_2024_select %>%
-  group_by(Company_Location_Name) %>%
-  summarise(
-    Avg_Salary_USD = round(mean(Salary_USD),0), Count = n(), .groups = 'drop') %>%
-  mutate(Percentage = round(Count / Total_Count_2024 * 100, 0)) %>%
-  arrange(desc(Avg_Salary_USD))
-International = data_salaries_2024_select %>%
+Summarise_Salary = function(df, group_cols) {
+  df %>%
+    group_by(across(all_of(group_cols))) %>%
+    summarise(
+      Avg_Salary_USD = round(mean(Salary_USD), 0),
+      Count = n(),
+      .groups = 'drop'
+    ) %>%
+    mutate(
+      Percentage = round(Count / Total_Count_2024 * 100, 2)
+    ) %>%
+    arrange(desc(Avg_Salary_USD))
+}
+Job_Title_Summary = Summarise_Salary(data_salaries_2024_select, 'Job_Title')
+Experience_Level_Summary = Summarise_Salary(data_salaries_2024_select, 'Experience_Level')
+Work_Time_Arrangement_Summary = Summarise_Salary(data_salaries_2024, 'Work_Time_Arrangement')
+Work_Office_Arrangement_Summary = Summarise_Salary(data_salaries_2024_select, 'Work_Office_Arrangement')
+Company_Size_Summary = Summarise_Salary(data_salaries_2024_select, 'Company_Size')
+Company_Location_Name_Summary = Summarise_Salary(data_salaries_2024_select, c('Company_Location_Name','Company_Location_Code3'))
+Title_Experience_Summary = Summarise_Salary(data_salaries_2024_select, c('Job_Title', 'Experience_Level'))
+Title_Time_Arrangement_Summary = Summarise_Salary(data_salaries_2024, c('Job_Title', 'Work_Time_Arrangement'))
+Experience_Time_Arrangement_Summary = Summarise_Salary(data_salaries_2024, c('Experience_Level', 'Work_Time_Arrangement'))
+Office_Time_Arrangement_Summary = Summarise_Salary(data_salaries_2024, c('Work_Office_Arrangement', 'Work_Time_Arrangement'))
+Company_Location_Size_Summary = Summarise_Salary(data_salaries_2024_select, c('Company_Location_Name', 'Company_Size'))
+International_Summary = data_salaries_2024 %>%
+  filter(Work_Time_Arrangement == 'Full-time') %>%
   group_by(International) %>%
   summarise(
     Avg_Salary_USD = round(mean(Salary_USD),0), Count = n(), .groups = 'drop') %>%
-  mutate(Percentage = round(Count / Total_Count_2024 * 100, 0)) %>%
+  mutate(Percentage = round(Count / Total_Count_2024 * 100, 2)) %>%
   arrange(desc(Avg_Salary_USD))
-Title_Experience = data_salaries_2024_select %>%
-  group_by(Job_Title, Experience_Level) %>%
-  summarise(
-    Avg_Salary_USD = round(mean(Salary_USD),0), Count = n(), .groups = 'drop') %>%
-  mutate(Percentage = round(Count / Total_Count_2024 * 100, 0)) %>%
-  arrange(desc(Avg_Salary_USD))
-Title_Time_Arrangement = data_salaries_2024_select %>%
-  group_by(Job_Title, Work_Time_Arrangement) %>%
-  summarise(
-    Avg_Salary_USD = round(mean(Salary_USD),0), Count = n(), .groups = 'drop') %>%
-  mutate(Percentage = round(Count / Total_Count_2024 * 100, 0)) %>%
-  arrange(desc(Avg_Salary_USD))
-Experience_Time_Arrangement = data_salaries_2024_select %>%
-  group_by(Experience_Level, Work_Time_Arrangement) %>%
-  summarise(
-    Avg_Salary_USD = round(mean(Salary_USD),0), Count = n(), .groups = 'drop') %>%
-  mutate(Percentage = round(Count / Total_Count_2024 * 100, 0)) %>%
-  arrange(desc(Avg_Salary_USD))
-Office_Time_Arrangement = data_salaries_2024_select %>%
-  group_by(Work_Office_Arrangement, Work_Time_Arrangement) %>%
-  summarise(
-    Avg_Salary_USD = round(mean(Salary_USD),0), Count = n(), .groups = 'drop') %>%
-  mutate(Percentage = round(Count / Total_Count_2024 * 100, 0)) %>%
-  arrange(desc(Avg_Salary_USD))
-Company_Location_Size = data_salaries_2024_select %>%
-  group_by(Company_Location_Name, Company_Size) %>%
-  summarise(
-    Avg_Salary_USD = round(mean(Salary_USD),0), Count = n(), .groups = 'drop') %>%
-  mutate(Percentage = round(Count / Total_Count_2024 * 100, 0)) %>%
-  arrange(desc(Avg_Salary_USD))
-International_by_Location = data_salaries_2024 %>%
+International_by_Location_Summary = data_salaries_2024 %>%
+  filter(International == 'International', Work_Time_Arrangement == 'Full-time') %>%
   group_by(International, Company_Location_Name, Employee_Residence_Name) %>%
   summarise(
     Avg_Salary_USD = round(mean(Salary_USD),0), Count = n(), .groups = 'drop') %>%
-  mutate(Percentage = round(Count / Total_Count_2024 * 100, 0)) %>%
-  filter(International == 'International') %>%
+  mutate(Percentage = round(Count / Total_Count_2024 * 100, 2)) %>%
   arrange(desc(Avg_Salary_USD))
-Job_Title_asc = data_salaries_2024_select %>%
-  group_by(Job_Title) %>%
-  summarise(
-    Avg_Salary_USD = round(mean(Salary_USD),0), Count = n(), .groups = 'drop') %>%
-  mutate(Percentage = round(Count / Total_Count_2024 * 100, 0)) %>%
-  arrange(Avg_Salary_USD)
-Company_Location_Name_asc = data_salaries_2024_select %>%
-  group_by(Company_Location_Name) %>%
-  summarise(
-    Avg_Salary_USD = round(mean(Salary_USD),0), Count = n(), .groups = 'drop') %>%
-  mutate(Percentage = round(Count / Total_Count_2024 * 100, 0)) %>%
-  arrange(Avg_Salary_USD)
-Title_Experience_asc = data_salaries_2024_select %>%
-  group_by(Job_Title, Experience_Level) %>%
-  summarise(
-    Avg_Salary_USD = round(mean(Salary_USD),0), Count = n(), .groups = 'drop') %>%
-  mutate(Percentage = round(Count / Total_Count_2024 * 100, 0)) %>%
-  arrange(Avg_Salary_USD)
-Title_Time_Arrangement_asc = data_salaries_2024_select %>%
-  group_by(Job_Title, Work_Time_Arrangement) %>%
-  summarise(
-    Avg_Salary_USD = round(mean(Salary_USD),0), Count = n(), .groups = 'drop') %>%
-  mutate(Percentage = round(Count / Total_Count_2024 * 100, 0)) %>%
-  arrange(Avg_Salary_USD)
-Company_Location_Size_asc = data_salaries_2024_select %>%
-  group_by(Company_Location_Name, Company_Size) %>%
-  summarise(
-    Avg_Salary_USD = round(mean(Salary_USD),0), Count = n(), .groups = 'drop') %>%
-  mutate(Percentage = round(Count / Total_Count_2024 * 100, 0)) %>%
-  arrange(Avg_Salary_USD)
-International_by_Location_asc = data_salaries_2024 %>%
-  group_by(International, Company_Location_Name, Employee_Residence_Name) %>%
-  summarise(
-    Avg_Salary_USD = round(mean(Salary_USD),0), Count = n(), .groups = 'drop') %>%
-  mutate(Percentage = round(Count / Total_Count_2024 * 100, 0)) %>%
-  filter(International == 'International') %>%
-  arrange(Avg_Salary_USD)
-  
+Job_Title_Summary_asc = Job_Title_Summary %>% arrange(Avg_Salary_USD)
+Company_Location_Name_Summary_asc = Company_Location_Name_Summary %>% arrange(Avg_Salary_USD)
+Title_Experience_Summary_asc = Title_Experience_Summary %>% arrange(Avg_Salary_USD)
+Title_Time_Arrangement_Summary_asc = Title_Time_Arrangement_Summary %>% arrange(Avg_Salary_USD)
+Company_Location_Size_Summary_asc = Company_Location_Size_Summary %>% arrange(Avg_Salary_USD)
+International_by_Location_Summary_asc = International_by_Location_Summary %>% arrange(Avg_Salary_USD)
 formattable(
-  data_salaries_2024_select,
+  head(data_salaries_2024_select,10),
   list(
     Salary_USD = formatter("span",
-                           style = x ~ style(
+                           style = x ~ list(
                              display = "block",
                              padding = "0 4px",
                              `border-radius` = "4px",
                              `background-color` = csscolor(gradient(as.numeric(x), "white", "lightblue"))
                            ),
-                           x ~ comma(x, accuracy = 1)
+                           format = x ~ comma(as.numeric(x), accuracy = 1)
     )
   )
 )
 formattable(
-  data_salaries_2024_select_asc,
+  head(data_salaries_2024_select_asc,10),
   list(
     Salary_USD = formatter("span",
-                           style = x ~ style(
+                           style = x ~ list(
                              display = "block",
                              padding = "0 4px",
                              `border-radius` = "4px",
                              `background-color` = csscolor(gradient(as.numeric(x), "white", "lightblue"))
                            ),
-                           x ~ comma(x, accuracy = 1)
+                           format = x ~ comma(as.numeric(x), accuracy = 1)
     )
   )
 )
@@ -268,21 +199,41 @@ Pay_Summary_by = function(df) {
 Top10_Pay_Summary_by = function(df) {
  Pay_Summary_by(head(df, 10))
 }
-Pay_Summary_by(Experience_Level)
-Pay_Summary_by(Work_Time_Arrangement)
-Pay_Summary_by(Company_Size)
-Pay_Summary_by(International)
-Pay_Summary_by(Experience_Time_Arrangement)
-Pay_Summary_by(Office_Time_Arrangement)
-Top10_Pay_Summary_by(Job_Title)
-Top10_Pay_Summary_by(Job_Title_asc)
-Top10_Pay_Summary_by(Company_Location_Name)
-Top10_Pay_Summary_by(Company_Location_Name_asc)
-Top10_Pay_Summary_by(Title_Experience)
-Top10_Pay_Summary_by(Title_Experience_asc)
-Top10_Pay_Summary_by(Title_Time_Arrangement)
-Top10_Pay_Summary_by(Title_Time_Arrangement_asc)
-Top10_Pay_Summary_by(Company_Location_Size)
-Top10_Pay_Summary_by(Company_Location_Size_asc)
-Top10_Pay_Summary_by(International_by_Location)
-Top10_Pay_Summary_by(International_by_Location_asc)
+Pay_Summary_by(Experience_Level_Summary)
+Pay_Summary_by(Work_Time_Arrangement_Summary)
+Pay_Summary_by(Company_Size_Summary)
+Pay_Summary_by(International_Summary)
+Pay_Summary_by(Experience_Time_Arrangement_Summary)
+Pay_Summary_by(Office_Time_Arrangement_Summary)
+Top10_Pay_Summary_by(Job_Title_Summary)
+Top10_Pay_Summary_by(Job_Title_Summary_asc)
+Top10_Pay_Summary_by(Company_Location_Name_Summary)
+Top10_Pay_Summary_by(Company_Location_Name_Summary_asc)
+Top10_Pay_Summary_by(Title_Experience_Summary)
+Top10_Pay_Summary_by(Title_Experience_Summary_asc)
+Top10_Pay_Summary_by(Title_Time_Arrangement_Summary)
+Top10_Pay_Summary_by(Title_Time_Arrangement_Summary_asc)
+Top10_Pay_Summary_by(Company_Location_Size_Summary)
+Top10_Pay_Summary_by(Company_Location_Size_Summary_asc)
+Top10_Pay_Summary_by(International_by_Location_Summary)
+Top10_Pay_Summary_by(International_by_Location_Summary_asc)
+library(plotly)
+World_Map_by_Salary = plot_ly(
+  data = Company_Location_Name_Summary,
+  type = 'choropleth',
+  locations = ~Company_Location_Code3,
+  z = ~Avg_Salary_USD,
+  text = ~Company_Location_Name,
+  colorscale = "Blues",
+  reversescale = TRUE,
+  locationmode = 'ISO-3'
+) %>%
+  layout(
+    title = 'Avg Salary by Company Location (USD)',
+    geo = list(
+      showframe = FALSE,
+      showcoastlines = TRUE,
+      projection = list(type = 'equirectangular')
+    )
+  )
+World_Map_by_Salary
