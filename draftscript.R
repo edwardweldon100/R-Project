d816e51e2bbbf107894566c0d0265dc66ea7558a
Ed Weldon <ed.j.weldon@gmail.com>
2025-08-05 22:32:44 -0400

    Cleanup. USA, Continent, Field Grouping and Histograms.  Box Plots

--- draftscript.R
+++ draftscript.R
@@ -2,6 +2,7 @@ library(dplyr)
 library(stringr)
 library(formattable)
 library(scales)
+library(countrycode)
 data_salaries = read.csv('salaries.csv')
 head(data_salaries)
 data_salaries[!complete.cases(data_salaries), ]
@@ -16,6 +17,7 @@ colnames(data_salaries)[which(names(data_salaries) == "Employee_Residence")] = "
 colnames(data_salaries)[which(names(data_salaries) == "Company_Location")] = "Company_Location_Code"
 colnames(data_salaries)[which(names(data_salaries) == "Experience_Level")] = "Experience_Level_Code"
 country_codes_legend = read.csv('continents2.csv')
+field_title_legend = read.csv('data_field_title_legend.csv')
 country_name_lookup = function(code) {
   code_match = match(code, country_codes_legend$alpha.2)
   country_codes_legend$name[code_match]
@@ -23,6 +25,9 @@ country_name_lookup = function(code) {
 country_code3_lookup = function(code2) {
   code_match = match(code2, country_codes_legend$alpha.2)
   country_codes_legend$alpha.3[code_match]
+}
+field_lookup = function(title) {
+  field_title_legend$Field[match(title, field_title_legend$Title)]
 }
 data_salaries = data_salaries %>%
   mutate(
@@ -44,12 +49,18 @@ data_salaries = data_salaries %>%
       ifelse(Company_Location_Code != Employee_Residence_Code, 'International', 'Domestic'),
       levels = c('Domestic', 'International')),
     Employee_Residence_Name = country_name_lookup(Employee_Residence_Code),
+    Field = factor(field_lookup(Job_Title), levels = c('Data Engineering', 'Data Science', 'Data Analysis', 'Business Intelligence', 'Machine Learning', 'Artificial Intelligence   (general)', 'Software Development   (general)', 'Research & Development   (general)')),
     Company_Location_Name = country_name_lookup(Company_Location_Code),
-    Company_Location_Code3 = country_code3_lookup(Company_Location_Code)
+    Company_Location_Code3 = country_code3_lookup(Company_Location_Code),
+    Continent = countrycode(sourcevar = Company_Location_Code3, origin = 'iso3c', destination = 'continent'),
+    USA = factor(
+      ifelse(Company_Location_Code3 == 'USA', 'USA', 'Outside_USA'),
+      levels = c('USA', 'Outside_USA'))
   )
 data_salaries = data_salaries %>% 
   select(
     Work_Year,
+    Field,
     Job_Title,
     Experience_Level_Code,
     Experience_Level,
@@ -61,6 +72,8 @@ data_salaries = data_salaries %>%
     Company_Location_Code,
     Company_Location_Code3,
     Company_Location_Name,
+    Continent,
+    USA,
     Employee_Residence_Code,
     Employee_Residence_Name,
     International,
@@ -73,9 +86,9 @@ yearly_job_count = data_salaries %>%
 yearly_job_count
 data_salaries_2024 = data_salaries %>% filter(Work_Year == 2024) %>%
   arrange(desc(Salary_USD))
-data_salaries_2024_FTonly = data_salaries %>% filter(Work_Time_Arrangement == 'Full-time')
+data_salaries_2024_FTonly = data_salaries_2024 %>% filter(Work_Time_Arrangement == 'Full-time')
 data_salaries_2024_select = data_salaries_2024 %>% filter(Work_Time_Arrangement == 'Full-time') %>% 
-  select(Job_Title, Experience_Level, Work_Time_Arrangement, Work_Office_Arrangement, Company_Size, Company_Location_Code3, Company_Location_Name, International, Salary_USD)
+  select(Field, Job_Title, Experience_Level, Work_Time_Arrangement, Work_Office_Arrangement, Company_Size, USA, Continent, Company_Location_Name, Company_Location_Code3, International, Salary_USD)
 data_salaries_2024_select_asc = data_salaries_2024_select %>% arrange(Salary_USD)
 Total_Count_2024 = nrow(data_salaries_2024)
 FX_Rates_2024 = data_salaries_2024 %>%
@@ -103,24 +116,28 @@ Summarise_Salary = function(df, group_cols) {
     ) %>%
     arrange(desc(Avg_Salary_USD))
 }
-Job_Title_Summary = Summarise_Salary(data_salaries_2024_select, 'Job_Title')
-Experience_Level_Summary = Summarise_Salary(data_salaries_2024_select, 'Experience_Level')
+Field_Summary = Summarise_Salary(data_salaries_2024_FTonly, 'Field')
+Field_Title_Summary = Summarise_Salary(data_salaries_2024_FTonly, c('Field', 'Job_Title'))
+Experience_Level_Summary = Summarise_Salary(data_salaries_2024_FTonly, 'Experience_Level')
 Work_Time_Arrangement_Summary = Summarise_Salary(data_salaries_2024, 'Work_Time_Arrangement')
-Work_Office_Arrangement_Summary = Summarise_Salary(data_salaries_2024_select, 'Work_Office_Arrangement')
-Company_Size_Summary = Summarise_Salary(data_salaries_2024_select, 'Company_Size')
-Company_Location_Name_Summary = Summarise_Salary(data_salaries_2024_select, c('Company_Location_Name','Company_Location_Code3'))
-Title_Experience_Summary = Summarise_Salary(data_salaries_2024_select, c('Job_Title', 'Experience_Level'))
-Title_Time_Arrangement_Summary = Summarise_Salary(data_salaries_2024, c('Job_Title', 'Work_Time_Arrangement'))
+Work_Office_Arrangement_Summary = Summarise_Salary(data_salaries_2024_FTonly, 'Work_Office_Arrangement')
+Company_Size_Summary = Summarise_Salary(data_salaries_2024_FTonly, 'Company_Size')
+Company_Location_Name_Summary = Summarise_Salary(data_salaries_2024_FTonly, c('Company_Location_Name','Company_Location_Code3'))
+Continent_Summary = Summarise_Salary(data_salaries_2024_FTonly, 'Continent')
+USA_Summary = Summarise_Salary(data_salaries_2024_FTonly, 'USA')
+Field_Experience_Summary = Summarise_Salary(data_salaries_2024_FTonly, c('Field', 'Experience_Level'))
+Field_Time_Arrangement_Summary = Summarise_Salary(data_salaries_2024, c('Field', 'Work_Time_Arrangement'))
 Experience_Time_Arrangement_Summary = Summarise_Salary(data_salaries_2024, c('Experience_Level', 'Work_Time_Arrangement'))
 Office_Time_Arrangement_Summary = Summarise_Salary(data_salaries_2024, c('Work_Office_Arrangement', 'Work_Time_Arrangement'))
-Company_Location_Size_Summary = Summarise_Salary(data_salaries_2024_select, c('Company_Location_Name', 'Company_Size'))
+Company_USA_Size_Summary = Summarise_Salary(data_salaries_2024_FTonly, c('USA', 'Company_Size'))
+Company_Continent_Size_Summary = Summarise_Salary(data_salaries_2024_FTonly, c('Continent', 'Company_Size'))
 International_Summary = Summarise_Salary(data_salaries_2024_FTonly, 'International')
-International_by_Location_Summary = Summarise_Salary(data_salaries_2024_FTonly, c('International','Company_Location_Name', 'Employee_Residence_Name'))
-Job_Title_Summary_asc = Job_Title_Summary %>% arrange(Avg_Salary_USD)
+International_by_Location_Summary = Summarise_Salary(data_salaries_2024_FTonly, c('International', 'Company_Location_Name', 'Employee_Residence_Name'))
+Field_Summary_asc = Field_Summary %>% arrange(Avg_Salary_USD)
+Field_Title_Summary_asc = Field_Title_Summary %>% arrange(Avg_Salary_USD)
+Field_Experience_Summary_asc = Field_Experience_Summary %>% arrange(Avg_Salary_USD)
+Field_Time_Arrangement_Summary_asc = Field_Time_Arrangement_Summary %>% arrange(Avg_Salary_USD)
 Company_Location_Name_Summary_asc = Company_Location_Name_Summary %>% arrange(Avg_Salary_USD)
-Title_Experience_Summary_asc = Title_Experience_Summary %>% arrange(Avg_Salary_USD)
-Title_Time_Arrangement_Summary_asc = Title_Time_Arrangement_Summary %>% arrange(Avg_Salary_USD)
-Company_Location_Size_Summary_asc = Company_Location_Size_Summary %>% arrange(Avg_Salary_USD)
 International_by_Location_Summary_asc = International_by_Location_Summary %>% arrange(Avg_Salary_USD)
 formattable(
   head(data_salaries_2024_select,10),
@@ -191,17 +208,19 @@ Top10_Pay_Summary_by = function(df) {
 Pay_Summary_by(Experience_Level_Summary)
 Pay_Summary_by(Work_Time_Arrangement_Summary)
 Pay_Summary_by(Company_Size_Summary)
+Pay_Summary_by(Continent_Summary)
+Pay_Summary_by(USA_Summary)
 Pay_Summary_by(International_Summary)
 Pay_Summary_by(Experience_Time_Arrangement_Summary)
 Pay_Summary_by(Office_Time_Arrangement_Summary)
-Top10_Pay_Summary_by(Job_Title_Summary)
-Top10_Pay_Summary_by(Job_Title_Summary_asc)
+Pay_Summary_by(Field_Summary)
+Pay_Summary_by(Field_Summary_asc)
+Pay_Summary_by(Field_Experience_Summary)
+Pay_Summary_by(Field_Time_Arrangement_Summary)
 Top10_Pay_Summary_by(Company_Location_Name_Summary)
 Top10_Pay_Summary_by(Company_Location_Name_Summary_asc)
-Top10_Pay_Summary_by(Title_Experience_Summary)
-Top10_Pay_Summary_by(Title_Experience_Summary_asc)
-Top10_Pay_Summary_by(Title_Time_Arrangement_Summary)
-Top10_Pay_Summary_by(Title_Time_Arrangement_Summary_asc)
+Top10_Pay_Summary_by(Field_Title_Summary)
+Top10_Pay_Summary_by(Field_Title_Summary_asc)
 Top10_Pay_Summary_by(Company_Location_Size_Summary)
 Top10_Pay_Summary_by(Company_Location_Size_Summary_asc)
 Top10_Pay_Summary_by(International_by_Location_Summary)
@@ -271,10 +290,10 @@ Data_Salary_Overall_Histogram
 Stacked_Histogram = function(df, catcol, numcol = 'Salary_USD') {
   plot_ly(
     data = df,
-    x = ~get(numcol),
-    color = ~get(catcol),
+    x = ~.data[[numcol]],
+    color = ~.data[[catcol]],
     type = "histogram",
-    opacity = .5
+    opacity = .7
   ) %>%
     layout(
       title = paste(catcol, 'Salary Distribution'),
@@ -283,7 +302,37 @@ Stacked_Histogram = function(df, catcol, numcol = 'Salary_USD') {
       barmode = "overlay"
     )
 }
+Box_1Lvl = function(df, lvl, numcol = 'Salary_USD') {
+  plot_ly(df, 
+          y = df[[numcol]], 
+          color = df[[lvl]], 
+          type = "box")
+}
+Box_2Lvl = function(df, lvl, subLvl, numcol = 'Salary_USD') {
+  plot_ly(df, 
+          x = df[[lvl]], 
+          y = df[[numcol]], 
+          color = df[[subLvl]], 
+          type = "box") %>%
+  layout(boxmode = "group")
+}
+Stacked_Histogram(data_salaries_2024_FTonly, 'Field')
 Stacked_Histogram(data_salaries_2024_FTonly, 'Experience_Level')
 Stacked_Histogram(data_salaries_2024, 'Work_Time_Arrangement')
 Stacked_Histogram(data_salaries_2024_FTonly, 'Company_Size')
+Stacked_Histogram(data_salaries_2024_FTonly, 'USA')
+Stacked_Histogram(data_salaries_2024_FTonly, 'Continent')
 Stacked_Histogram(data_salaries_2024_FTonly, 'International')
+Box_1Lvl(data_salaries_2024_FTonly, 'Field')
+Box_1Lvl(data_salaries_2024_FTonly, 'Experience_Level')
+Box_1Lvl(data_salaries_2024, 'Work_Time_Arrangement')
+Box_1Lvl(data_salaries_2024_FTonly, 'Company_Size')
+Box_1Lvl(data_salaries_2024_FTonly, 'USA')
+Box_1Lvl(data_salaries_2024_FTonly, 'Continent')
+Box_1Lvl(data_salaries_2024_FTonly, 'International')
+Box_2Lvl(data_salaries_2024_FTonly, 'Field', 'Experience_Level')
+Box_2Lvl(data_salaries_2024, 'Field', 'Work_Time_Arrangement')
+Box_2Lvl(data_salaries_2024, 'Experience_Level', 'Work_Time_Arrangement')
+Box_2Lvl(data_salaries_2024, 'Work_Office_Arrangement', 'Work_Time_Arrangement')
+Box_2Lvl(data_salaries_2024_FTonly, 'Continent', 'Company_Size')
+Box_2Lvl(data_salaries_2024_FTonly, 'USA', 'Company_Size')
